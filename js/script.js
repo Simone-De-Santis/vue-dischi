97616bcb000dec7d7bc1b54a7ff795e338873dd8
Simone de santis <82234672+Simone-De-Santis@users.noreply.github.com>
2021-09-03 09:24:09 +0200

    compilazione disco dianmica

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
-  methods: {},
+  data: {
+    diskList: []
+  },
+  methods: {
+  },
+  created() {
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+
+      const disk = res.data.response
+
+      this.diskList.push(disk);
+
+      console.log(this.diskList)
+
+    })
+  }
 });
